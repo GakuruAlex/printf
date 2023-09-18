f7e0f4d608baa25e476aeb36e4179b9fcd4c29de
alexgicimo <gakurualexgicimo@gmail.com>
2023-09-18 23:12:07 +0000

    Print hexadecimals

--- print_hexadecimal.c
+++ print_hexadecimal.c
@@ -1,8 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 
-int print_hexadecimal(int element)
+
+int print_hexadecimal(unsigned int value)
 {
-    return printf("%x", element);
-}
+int count = 0, i;
+int digits[8];
 
+  for ( i = 0; i < 8; i++) {
+    digits[i] = value & 0xF;
+    value >>= 4;
+  }
+
+  
+  for ( i = 7; i >= 0; i--) {
+    char digit = digits[i];
+    if (digit < 10) {
+      _putchar('0' + digit);
+    } else {
+      _putchar('A' + digit - 10);
+    }
+    count++;
+  }
+
+  return count;
+}
