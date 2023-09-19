b48b1cb5fae57f2f19261327ab71b8c98392ea89
alexgicimo <gakurualexgicimo@gmail.com>
2023-09-19 05:23:03 +0000

    print unsigned int

--- print_unsigned.c
+++ print_unsigned.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 
-int print_unsigned(unsigned int value)
-{
-    return printf("%u", value);
+int print_unsigned(unsigned int value) {
+  int length = 0;
+
+  if (value >= 10) {
+    length += print_unsigned(value / 10);
+  }
+
+  _putchar((value % 10) + '0');
+  length++;
+
+  return length;
 }
