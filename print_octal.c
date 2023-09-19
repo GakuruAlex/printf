29a43e40278c8d988b470c78756b8f96ca9d153c
alexgicimo <gakurualexgicimo@gmail.com>
2023-09-19 05:39:43 +0000

    print unsigned octal

--- print_octal.c
+++ print_octal.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 
-int print_octal(unsigned int value)
-{
-    return printf("%o", value);
+int print_octal(unsigned int value) {
+  int length = 0;
+
+  if (value >= 8) {
+    length += print_octal(value / 8);
+  }
+
+  _putchar((value % 8) + '0');
+  length++;
+
+  return length;
 }
