a00dc0495edc66982b181b31918cd15795196259
Clifford Owiti <cliffordowiti@gmail.com>
2023-09-20 01:04:33 -0700

    Undoing the NULL part

--- _printf.c
+++ _printf.c
@@ -11,8 +11,6 @@ int _printf(const char *format, ...)
     va_list format_arg;
 
     va_start(format_arg, format);
-    if (format == NULL)
-	    return (-1);
     for (i = 0; format[i] != '\0'; i++)
     {
         if (format[i] != '%')
