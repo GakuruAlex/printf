0fd11b0f440faf728d418294c6acdcf405e3a95e
Clifford Owiti <cliffordowiti@gmail.com>
2023-09-20 03:14:19 -0700

    Betty Check

--- check_hex.c
+++ check_hex.c
@@ -1,19 +1,24 @@
 #include "main.h"
 #define FALSE 0
 #define TRUE 1
+/**
+ * is_hexadecimal - A function that checks for hexadecimals
+ * @value: The input
+ * Return: True or False
+ */
 bool is_hexadecimal(int value)
 {
-
-  if (value < 0) {
-    return FALSE;
-  }
-
-  while (value > 0) {
-    if (!isxdigit(value % 16)) {
-      return FALSE;
-    }
-    value /= 16;
-  }
-
-  return TRUE;
+	if (value < 0)
+	{
+		return (FALSE);
+	}
+	while (value > 0)
+	{
+		if (!isxdigit(value % 16))
+		{
+			return (FALSE);
+		}
+		value /= 16;
+	}
+	return (TRUE);
 }
